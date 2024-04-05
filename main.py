c9e7cc84a8c9587e0ea94e36efedc2e5642f4b42
sundeepmeduri <sundeepmeduri@outlook.com>
2024-04-05 11:55:58 -0500

    Initial commit 3

--- main.py
+++ main.py
@@ -1,40 +1,76 @@
-from fastapi import HTTPException, status, Security, FastAPI
-from fastapi.security import APIKeyHeader, APIKeyQuery
-
-api_keys = [
-    "my_api_key"
-]
-
-api_key_header = APIKeyHeader(name="x-api-key", auto_error=False)
-
-
-def get_api_key(
-        api_key_header: str = Security(api_key_header),
-) -> str:
-    if api_key_header in api_keys:
-        return api_key_header
-    raise HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Invalid or missing API Key",
-    )
-
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
 
-@app.get("/public")
-def public():
-    """A public endpoint that does not require any authentication."""
-    return "Public Endpoint"
+BOOKS = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
+]
 
 
-@app.get("/protected")
-def private(api_key: str = Security(get_api_key)):
-    """A private endpoint that requires a valid API key to be provided."""
-    return f"Private Endpoint. API Key: {api_key}"
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
 
 
-if __name__ == "__main__":
-    import uvicorn
+@app.get("/books/{book_title}")
+async def read_book(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+
+# Get all books from a specific author using path or query parameters
+@app.get("/books/byauthor/")
+async def read_books_by_author_path(author: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == author.casefold():
+            books_to_return.append(book)
+
+    return books_to_return
+
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() and \
+                book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+
+    return books_to_return
+
+
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
+
+
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[i] = updated_book
+
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
