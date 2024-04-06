327f36dafb889d0f6d7996008cd8ef0881d376b8
sundeepmeduri <sundeepmeduri@outlook.com>
2024-04-06 10:10:30 -0500

    Path parameters

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from fastapi import Body, FastAPI
 
 app = FastAPI()
 
-
 BOOKS = [
     {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
     {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
@@ -13,64 +12,28 @@ BOOKS = [
 ]
 
 
-@app.get("/books")
+@app.get('/books')
 async def read_all_books():
     return BOOKS
 
 
-@app.get("/books/{book_title}")
-async def read_book(book_title: str):
+# example of path parameter
+@app.get('/books/author/{author}')
+async def get_author_books(author: str):
+
+    data = dict()
+    book_list = list()
+
     for book in BOOKS:
-        if book.get('title').casefold() == book_title.casefold():
-            return book
+        if book['author'].lower() == author.lower():
+            book_list.append(book)
+
+    data['author'] = author
+    data['Books'] = book_list
+
+    return data
 
 
-@app.get("/books/")
-async def read_category_by_query(category: str):
-    books_to_return = []
-    for book in BOOKS:
-        if book.get('category').casefold() == category.casefold():
-            books_to_return.append(book)
-    return books_to_return
-
-
-# Get all books from a specific author using path or query parameters
-@app.get("/books/byauthor/")
-async def read_books_by_author_path(author: str):
-    books_to_return = []
-    for book in BOOKS:
-        if book.get('author').casefold() == author.casefold():
-            books_to_return.append(book)
-
-    return books_to_return
-
-
-@app.get("/books/{book_author}/")
-async def read_author_category_by_query(book_author: str, category: str):
-    books_to_return = []
-    for book in BOOKS:
-        if book.get('author').casefold() == book_author.casefold() and \
-                book.get('category').casefold() == category.casefold():
-            books_to_return.append(book)
-
-    return books_to_return
-
-
-@app.post("/books/create_book")
-async def create_book(new_book=Body()):
-    BOOKS.append(new_book)
-
-
-@app.put("/books/update_book")
-async def update_book(updated_book=Body()):
-    for i in range(len(BOOKS)):
-        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
-            BOOKS[i] = updated_book
-
-
-@app.delete("/books/delete_book/{book_title}")
-async def delete_book(book_title: str):
-    for i in range(len(BOOKS)):
-        if BOOKS[i].get('title').casefold() == book_title.casefold():
-            BOOKS.pop(i)
-            break
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run('main:app', host="0.0.0.0", port=8000, reload=True)
